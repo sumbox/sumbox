9cb6ba87b32879817166f06d05ead09bbee0482c
Johannes Van Zijl <ysterbal@gmail.com>
2022-10-26 02:17:21 +0000

    Cleaned up code, added test example for the db call so we can mock the services.

--- backend/src/api/ideas.rs
+++ backend/src/api/ideas.rs
@@ -1,11 +1,11 @@
-use axum::{Router, Extension};
-use axum::extract::{Json};
+use axum::extract::Json;
+use axum::{Extension, Router};
 
-use serde::{Deserialize};
 use axum::routing::get;
 use axum::routing::post;
+use serde::Deserialize;
 
-use common::{AppError};
+use common::AppError;
 
 type AppResult<T> = Result<T, AppError>;
 type AppJsonResult<T> = AppResult<Json<T>>;
@@ -14,41 +14,49 @@ type Database = Extension<std::sync::Arc<db::PrismaClient>>;
 use crate::db::{self, idea};
 
 #[derive(Deserialize)]
-pub struct IdeaRequest { field1: String, field2: String }
+pub struct IdeaRequest {
+    field1: String,
+    field2: String,
+}
 
 pub fn create_route() -> Router {
     Router::new()
-        .route("/idea", get(get_idea))
-        .route("/idea", post(create_idea))
+        .route("/idea", get(read_idea))
+        .route("/idea", post(create_update_idea))
 }
 
-pub async fn get_idea(db:Database) -> AppResult<Json<Vec<idea::Data>>> {
-    let ideas = db
-    .idea()
-    .find_many(vec![])
-    .exec()
-    .await?;
+pub async fn read_idea(db: Database) -> AppResult<Json<Vec<idea::Data>>> {
+    let ideas: Vec<idea::Data> = db.idea().find_many(vec![]).exec().await?;
     Ok(Json::from(ideas))
 }
 
-pub async fn create_idea(db:Database, Json(input): Json<IdeaRequest>) -> AppJsonResult<idea::Data> {
-        let data : idea::Data = db
+// POST/PUT
+pub async fn create_update_idea(
+    db: Database,
+    Json(input): Json<IdeaRequest>,
+) -> AppJsonResult<idea::Data> {
+    let data: idea::Data = db
         .idea()
         .create(input.field1, input.field2, vec![])
         .exec()
         .await?;
-        println!("{:?}", data);
-        Ok(Json::from(data))
+    println!("{:?}", data);
+    Ok(Json::from(data))
 }
-
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
+    // Add tests here
+    #[tokio::test]
+    async fn it_works() {
+        let client = std::sync::Arc::new(db::new_client().await.unwrap());
 
-    // Add tests here 
-    // #[test]
-    // fn it_works() { 
-    //     assert_eq!(1,1);
-    // }
+        #[cfg(debug)]
+        client._db_push(false).await.unwrap();
+
+        let ideas: Vec<idea::Data> = client.idea().find_many(vec![]).exec().await.unwrap();
+        print!("Ideas {:?}", ideas);
+        assert_eq!(ideas.len(), 0);
+    }
 }
